04b7a920a78eb695f3cfd8652edfd1711a40db40
Marina Horuzhaya <marinakh192@gmail.com>
2023-07-30 23:18:06 +0300

    added //TODO implements

--- src/test/java/guru/qa/YandexTest.java
+++ src/test/java/guru/qa/YandexTest.java
@@ -1,16 +1,13 @@
 package guru.qa;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class YandexTest {
 
 
     @Test
     void yandexTest() {
+        //TODO implement
 
     }
 }
