25f77a6496c94878e6dedc10dc814e35b56ffed8
Marina Horuzhaya <marinakh192@gmail.com>
2023-07-30 22:20:27 +0300

    change Yandex test

--- src/test/java/guru/qa/YandexTest.java
+++ src/test/java/guru/qa/YandexTest.java
@@ -7,13 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class YandexTest {
-    static {
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        Configuration.browserCapabilities = capabilities;
-    }
+
 
     @Test
     void yandexTest() {
