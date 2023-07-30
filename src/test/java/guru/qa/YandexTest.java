9f5a11131c91832bdbb35ed025b72ddfd411c0c3
Marina Horuzhaya <marinakh192@gmail.com>
2023-07-30 22:59:21 +0300

    open yandex.by

--- src/test/java/guru/qa/YandexTest.java
+++ src/test/java/guru/qa/YandexTest.java
@@ -1,16 +1,14 @@
 package guru.qa;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class YandexTest {
 
 
     @Test
     void yandexTest() {
-
+    Selenide.open("https://yandex.by/");
     }
 }
